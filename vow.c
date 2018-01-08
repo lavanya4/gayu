8a879c2ecb5e63436b8f7a9b590584f2fc67904e
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-01-08 16:55:53 +0530

    vow.c

---
+++ vow.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+if(n>="A"&& n<="Z"|| n>="a"&& n<="z")
+{
+ if(n=="A"||n=="a"||n=="E"||n=="e"||n=="I"||n=="i"||n=="O"||n=="o"||n=="U"||n=="u")
+ {
+ printf("vowels")
+ else
+ {
+ printf("consonants")
+ }
+ }
+ }
+ 
