b3d8dbfb31d2d5cbf51ed5623f504661fa81aacb
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-01-08 17:00:30 +0530

    vow.c

--- vow.c
+++ vow.c
@@ -2,15 +2,15 @@
 int main()
 {
 int n;
-if(n>="A"&& n<="Z"|| n>="a"&& n<="z")
+if(n>="A" && n<="Z"|| n>="a" && n<="z")
 {
  if(n=="A"||n=="a"||n=="E"||n=="e"||n=="I"||n=="i"||n=="O"||n=="o"||n=="U"||n=="u")
  {
- printf("vowels")
+ printf("vowels");
  else
  {
- printf("consonants")
- }
+ printf("consonants) ;
+        }
  }
  }
  
