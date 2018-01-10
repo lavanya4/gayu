c4e43b469dc81962965d15a043897c288bdf3ebb
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-01-10 16:05:13 +0530

    year.c

--- year.c
+++ year.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
-void main()
+int main(void)
 {
 int n;
+  printf("enter the value");
+  scanf("%d",&n);
 if(n%4==0)
 {
 printf("it is a leap year");
-else
+}
+  else
 {
 printf("it is not a leap year");
 }
-}
+return 0;
 }
