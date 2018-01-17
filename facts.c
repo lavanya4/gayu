27755af66cce89c71110b0145d16d01a08c1d26e
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-01-17 16:54:28 +0530

    facts.c

---
+++ facts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n,fact;
+printf("enter the number");
+scanf("%d",&n);
+if(n==0||n==1)
+{
+printf("the factorial of a number");
+}
+else
+{
+return fact(n*(n-1))
+}
+}
