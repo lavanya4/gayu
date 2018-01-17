690c33d2657fd0e76d81a7f2dd43a217d402c193
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-01-17 16:02:26 +0530

    sums.c

---
+++ sums.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,k;
+int i,a[n],sum;
+printf("enter the values of n and k");
+scanf("%d""%d",&n&k);
+printf("enter the values of array"):
+scanf("%d",&a[n]);
+for(i=0;i<n;i++)
+{
+for(i=0;i<k;i++)
+{
+for(i=0;i<a[n];i++)
+}
+}
+}
