862e08c8a14aad9d4c64ee348bdf2f71a52e47e5
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-01-22 16:25:44 +0530

    prime.c

---
+++ prime.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n,i,a=0;
+printf("enter the value");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+a++;
+}
+if(a=2)
+{
+printf("\n prime");
+}
+else
+{
+printf("\n not prime");
+}
+return 0;
+}
